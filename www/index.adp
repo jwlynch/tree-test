b2ad382c91b911d72122f32683cd123265e098ed
Jim Lynch <jim@jam.sessionsnet.org>
2013-11-15 10:18:28 +0400

    some user interface, make-new-node

---
+++ www/index.adp
@@ -0,0 +1,9 @@
+<master>
+
+<formtemplate id="new_node"></formtemplate>
+
+<ul>
+  <multiple id="nodes">
+    <li>@nodes.tree_node_name@ </li>
+  </multiple>
+</ul>
---
+++ www/index.tcl
@@ -0,0 +1,47 @@
+ad_page_contract {
+} -query {
+} -properties {
+}
+
+set user_id [ad_conn user_id]
+set package_id [ad_conn package_id]
+set peeraddr [ad_conn peeraddr]
+
+db_multirow nodes get_nodes {
+    select
+        n.tree_node_name,
+        n.tree_node_id,
+        n.parent_id
+    from 
+        tree_nodes n
+    order by
+        n.tree_node_name
+}
+
+ad_form -name new_node -form {
+    tree_node_id:key
+    {tree_node_name:text}
+} -new_data {
+    db_transaction {
+	db_1row make_new_node {
+	    select
+	        tree_node__new
+	        (
+	            :tree_node_name,
+                    :tree_node_id,
+                    NULL, -- parent id
+
+                    :user_id,
+		    now(),
+		    :package_id,
+		    :peeraddr,
+		    :package_id -- context_id
+	        )
+	}
+    }
+
+    ad_returnredirect .
+    ad_script_abort
+}
+
+
